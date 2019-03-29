90d09ecc18849ef5c362995c33063e2a21dcd6e6
Projit <projit1234@hotmail.com>
2019-03-29 23:06:02 +0530

    hardcoded number of pictures n, n outputs successful

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import os
 import math
-import logging
 import argparse
 from typing import Union, List, Optional, Tuple
 import cv2
@@ -10,9 +9,62 @@ import scipy.sparse.csgraph as csgraph
 import matplotlib.pyplot as plt
 from img_stitch import ImageStitcher
 from stitch import _StitchImage
+from sklearn.cluster import SpectralClustering
+
+def find_matches(im1, im2, matcher):
+    matches = matcher.knnMatch(im1.feat, im2.feat, k=2)
+    good = [i for i, j in matches if i.distance < 0.7 * j.distance]
+    return len(good), len(matches)
 
 def find_clusters(inputs):
-    return [inputs, inputs]
+    marking = {}
+    for i,image in enumerate(inputs):
+        if isinstance(image, str):
+            img = cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2RGBA)
+        if img.shape[-1] == 3:
+            img = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
+
+        fname = os.path.splitext(os.path.split(image)[1])[0]
+        image_f = _StitchImage(img, name=fname)
+        # find features
+        image_f.find_features()
+        marking[i] = image_f
+    
+    edge_matrix = []
+    matcher = cv2.BFMatcher_create(cv2.NORM_L2)
+    for i in range(len(marking)):
+        temp = []
+        for j in range(len(marking)):
+            good, matches = find_matches(marking[i], marking[j], matcher)
+            if matches == 0:
+                temp.append(0)
+            else:
+                if good > 0.1 * matches:
+                    temp.append(1)
+                else:
+                    temp.append(0)
+        edge_matrix.append(temp)
+    adjacency_matrix = np.array(edge_matrix)
+    sc = SpectralClustering(3, affinity='precomputed', n_init=100)
+    sc.fit(adjacency_matrix)
+    groups = {}
+    for i in range(len(sc.labels_)):
+        try:
+            groups[sc.labels_[i]].append(i)
+        except:
+            groups[sc.labels_[i]] = [i]
+    clusters = []
+    for g in groups:
+        clusters.append([inputs[x] for x in groups[g]])
+    print("@@@@@@@@@@@@@@@@@@@@@@")
+    print(clusters)
+    # strf = ''
+    # for i in edge_matrix:
+        # for j in i:
+            # strf += str(j) + ' '
+        # strf += '\n'
+    # print(strf)
+    return clusters
 
 def run():
     ''' Driving Function + Argument Handling
@@ -40,9 +92,6 @@ def run():
             stitch.add_image(infile)
         result = stitch.stitch()
         cv2.imwrite(str(i) + "_" + args.output, cv2.cvtColor(result, cv2.COLOR_RGBA2BGRA))
-
-    # for infile in args.input:
-        # stitch.add_image(infile)
     
 if __name__ == '__main__':
     run()
